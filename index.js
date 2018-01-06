5ff83b8f6f820be06fd6b3b4c4068099b6ff4cb0
Clay Miller <clay@smockle.com>
2018-01-05 23:53:23 -0800

    Enable Prettier

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Strava
-require('dotenv-safe').load();
-var strava = require('strava-v3');
+require("dotenv-safe").load();
+var strava = require("strava-v3");
 
-strava.athlete.get({},function(err,payload,limits) {
-    if(!err) {
-        console.log(payload);
-    }
-    else {
-        console.log(err);
-    }
+strava.athlete.get({}, function(err, payload, limits) {
+  if (!err) {
+    console.log(payload);
+  } else {
+    console.log(err);
+  }
 });
