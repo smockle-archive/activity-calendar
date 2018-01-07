ad02abb65575cba3d9b601c43f351fae3ad14cdf
Clay Miller <clay@smockle.com>
2018-01-06 18:52:07 -0800

    60 minutes is an impossible number of minutes

--- bin/index.mjs
+++ bin/index.mjs
@@ -4,9 +4,8 @@ import { activities } from '../data/activities'
 import toEvent from '../lib/to-event'
   ;(async function () {
   const events = await Promise.all(activities.map(toEvent))
-  console.log(events)
-  // createCalendar(events, 'smockle/ics', (error, value) => {
-  //   if (error) throw error
-  //   console.log(value)
-  // })
+  createCalendar(events, 'smockle/ics', (error, value) => {
+    if (error) throw error
+    console.log(value)
+  })
 })()
--- test/to-date-array.mjs
+++ test/to-date-array.mjs
@@ -1,10 +1,18 @@
 import toDateArray from '../lib/to-date-array'
 import test from 'tape'
 
-test('toDateArray', t => {
+test('toDateArray without adjustment', t => {
   t.plan(1)
   const utcdate = '2018-01-02T16:38:40Z'
   const expected = [2018, 1, 2, 8, 40]
+  const actual = toDateArray(utcdate)
+  t.deepEqual(actual, expected, 'default')
+})
+
+test('toDateArray with adjustment', t => {
+  t.plan(1)
+  const utcdate = '2015-06-12T03:58:28Z'
+  const expected = [2015, 6, 11, 21, 0]
   const actual = toDateArray(utcdate)
   t.deepEqual(actual, expected, 'default')
 })
