eaf6ffa3eef4e8343ddd96185c5c8847731b30e3
Clay Miller <clay@smockle.com>
2018-03-18 00:47:11 -0700

    WIP

--- src/bin/index.mjs.ts
+++ src/bin/index.mjs.ts
@@ -1,19 +1,55 @@
-import serverless from "serverless-http";
-import express from "express";
-import ingest from "./routes/ingest";
-import bodyParser from "body-parser";
+import { Handler, Context, Callback } from "aws-lambda";
+import { round } from "lodash";
+import * as request from "request-promise";
+import { toMiles } from "../lib/to-miles.mjs";
 import { load as dotenv } from "dotenv-safe";
 dotenv();
 
-const app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(bodyParser.json());
-app.post("/ingest", ingest);
-app.listen(process.env.PORT, () =>
-  console.log(`Listening on port ${process.env.PORT}`)
-);
-export const handler = serverless(app);
+export interface HandlerRequest {
+  [key: string]: any;
+  body: {
+    [key: string]: any;
+    createdAt: string;
+    name: string;
+    distanceMeters: string;
+    elapsedTimeInSeconds: string;
+  };
+}
+
+export interface HandlerResponse {
+  statusCode: number;
+  body: string;
+}
+
+export const handler: Handler = (
+  event: HandlerRequest,
+  context: Context,
+  callback: Callback
+) => {
+  // Clean incoming data (from IFTTT)
+  const { createdAt, name, distanceMeters, elapsedTimeInSeconds } = event.body;
+  const distanceMiles: number = round(toMiles(parseFloat(distanceMeters)), 1);
+  const elapsedTimeInMinutes: number = round(
+    parseInt(elapsedTimeInSeconds, 10) / 60
+  );
+  // Prepare outgoing data
+  const { IFTTT_EVENT, IFTTT_KEY } = process.env;
+  const options = {
+    method: "POST",
+    uri: `https://maker.ifttt.com/trigger/${IFTTT_EVENT}/with/key/${IFTTT_KEY}`,
+    body: {
+      value1: `${name} (${distanceMiles} mi) ${createdAt} for ${elapsedTimeInMinutes} minutes`
+    },
+    json: true
+  };
+  // Send data to IFTTT
+  const response: HandlerResponse = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Message sent!"
+    })
+  };
+  request(options)
+    .then(() => callback(null, response))
+    .catch(error => callback(error));
+};
