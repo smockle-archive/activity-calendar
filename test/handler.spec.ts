6352b3826cc5d74f6169d3ace599859e4fd5906c
Clay Miller <clay@smockle.com>
2018-03-26 20:51:17 -0700

    Test failure to send message and test request options

--- test/handler.spec.ts
+++ test/handler.spec.ts
@@ -1,3 +1,7 @@
+jest.mock("request-promise");
+jest.mock("dotenv-safe");
+import { post } from "request-promise";
+import { load as dotenv } from "dotenv-safe";
 import { promisify } from "util";
 import {
   handler as _handler,
@@ -5,8 +9,32 @@ import {
   HandlerResponse
 } from "../src/bin/index.mjs";
 const handler = promisify(_handler);
+dotenv();
 
 describe("handler", () => {
+  test("prepares outgoing data", () => {
+    const event: HandlerRequest = {
+      body: {
+        createdAt: "March 17, 2018 at 02:00PM",
+        name: "Afternoon Run",
+        distanceMeters: "21375.5",
+        elapsedTimeInSeconds: "7515"
+      }
+    };
+    const expected = {
+      body: {
+        value1:
+          "Afternoon Run (13.3 mi) March 17, 2018 at 02:00PM for 125 minutes"
+      },
+      json: true,
+      method: "POST",
+      uri: "https://maker.ifttt.com/trigger/IFTTT_EVENT/with/key/IFTTT_KEY"
+    };
+    (post as any).mockResolvedValueOnce();
+    handler(event, null).then(() => {
+      expect((post as any).mock.calls[0][0]).toEqual(expected);
+    });
+  });
   test("sends message successfully", () => {
     const event: HandlerRequest = {
       body: {
@@ -22,12 +50,32 @@ describe("handler", () => {
         message: "Message sent!"
       })
     };
+    (post as any).mockResolvedValueOnce();
     handler(event, null)
       .then((data: HandlerResponse | null) => {
         expect(data).toEqual(expected);
       })
       .catch((error: Error | null) => {
         expect(error).toBeFalsy();
+      });
+  });
+  test("fails to send message", () => {
+    const event: HandlerRequest = {
+      body: {
+        createdAt: "March 17, 2018 at 02:00PM",
+        name: "Afternoon Run",
+        distanceMeters: "21375.5",
+        elapsedTimeInSeconds: "7515"
+      }
+    };
+    const expected = new Error("HI CLAY");
+    (post as any).mockRejectedValueOnce(expected);
+    handler(event, null)
+      .then((data: HandlerResponse | null) => {
+        expect(data).toBeFalsy();
+      })
+      .catch((error: Error | null) => {
+        expect(error).toEqual(expected);
       });
   });
 });
