9cf6c4eb4f384d40eac13da5c2537d3c46b8972f
Clay Miller <clay@smockle.com>
2018-03-27 22:53:25 -0700

    Parse event.body before destructuring

--- src/bin/index.mjs.ts
+++ src/bin/index.mjs.ts
@@ -7,25 +7,27 @@ dotenv();
 
 export interface HandlerRequest {
   [key: string]: any;
-  body: {
-    /** The activity start time, e.g "March 17, 2018 at 02:00PM" */
-    createdAt: string;
-    /** The activity distance, in meters, e.g. "21375.5" */
-    distanceMeters: string;
-    /** The activity duration, in seconds, e.g. "7515" */
-    elapsedTimeInSeconds: string;
-    /** The activity name, e.g. "Afternoon Run" */
-    name: string;
-    /** The activity type, e.g. "Run" */
-    activityType?: string;
-    /** The activity duration, in hours, minutes and seconds, e.g. "2 hours, 5 minutes, 15 seconds" */
-    elapsedTime?: string;
-    /** The activity URL on the Strava website, e.g. "http://www.strava.com/activities/1446540000" */
-    linkToActivity?: string;
-    /** The activity route image URL on the Strava website */
-    routeMapImageURL?: string;
-    [key: string]: any;
-  };
+  body: string; // HandlerRequestBody
+}
+
+export interface HandlerRequestBody {
+  /** The activity start time, e.g "March 17, 2018 at 02:00PM" */
+  createdAt: string;
+  /** The activity distance, in meters, e.g. "21375.5" */
+  distanceMeters: string;
+  /** The activity duration, in seconds, e.g. "7515" */
+  elapsedTimeInSeconds: string;
+  /** The activity name, e.g. "Afternoon Run" */
+  name: string;
+  /** The activity type, e.g. "Run" */
+  activityType?: string;
+  /** The activity duration, in hours, minutes and seconds, e.g. "2 hours, 5 minutes, 15 seconds" */
+  elapsedTime?: string;
+  /** The activity URL on the Strava website, e.g. "http://www.strava.com/activities/1446540000" */
+  linkToActivity?: string;
+  /** The activity route image URL on the Strava website */
+  routeMapImageURL?: string;
+  [key: string]: any;
 }
 
 export interface HandlerResponse {
@@ -39,7 +41,8 @@ export const handler: Handler = (
   callback: Callback
 ) => {
   // Clean incoming data (from IFTTT)
-  const { createdAt, distanceMeters, elapsedTimeInSeconds, name } = event.body;
+  const body: HandlerRequestBody = JSON.parse(event.body);
+  const { createdAt, distanceMeters, elapsedTimeInSeconds, name } = body;
   const distanceMiles: number = round(toMiles(parseFloat(distanceMeters)), 1);
   const elapsedTimeInMinutes: number = round(
     parseInt(elapsedTimeInSeconds, 10) / 60
